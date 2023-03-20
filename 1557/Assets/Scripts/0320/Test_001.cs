6d5ca4b42321f1785fc6c00a225a4f95360355d6
rlawnghks <jsend@naver.com>
2023-03-20 23:03:52 +0900

    0320 커밋

---
+++ 1557/Assets/Scripts/0320/Test_001.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_001 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int age;
+        age = 30;
+        Debug.Log(age);
+
+        float heigh1 = 160.5f;
+        float height2;
+        height2 = heigh1;
+        Debug.Log(height2);
+
+        string name;
+        name = "Sera";
+        Debug.Log(name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
